a8c922606e147c0ea53986bca73f5037f67c1925
Andrés Sánchez <sedran@outlook.com>
2018-11-26 21:51:38 -0300

    user defined time and custom function for each sentece

--- example/acon.js
+++ example/acon.js
@@ -12,30 +12,42 @@ var Acon = {
         for (let i = 0; i < sentences.length; i++) {
             let iDirection = 'bottom',
                 fDirection = 'bottom',
-                text, sentence, timeToRead, words, readDelay;
+                text = document.createElement('div'),
+                sentence, timeToRead, words, readDelay;
+
+            text.id = 'sentence-' + [i];
+            text.addEventListener('animationend', function () { Acon.destroySentence(this) }, false);
 
             if (sentences[i].constructor === Array) {
                 sentence = sentences[i][0];
-                iDirection = sentences[i][1];
+
+                for (let o = 1; o < sentences[i].length; o++) {
+                    if (typeof sentences[i][o] == 'number') {
+                        timeToRead = sentences[i][o];
+                    } else if (/\(.*\)/.test(sentences[i][o])){
+                        text.addEventListener('animationstart', function () { Acon.initCustomFunction(sentences[i][o]) }, false);
+                    }
+                    else {
+                        iDirection = sentences[i][o];
+                    }
+                }
+                
             } else {
                 sentence = sentences[i];
             }
+            
+            text.innerHTML = sentence;
 
             if (sentences[i + 1] && sentences[i + 1].constructor === Array) {
                 fDirection = sentences[i + 1][1];
             }
-
-            text = document.createElement('div');
-            text.addEventListener('animationend', function () { Acon.destroySentence(this) }, false);
-
-            text.id = 'sentence-' + [i];
-            text.innerHTML = sentence;
-
-            timeToRead;
-            words = sentence.split(' ').length;
-            timeToRead = words * 300;
-            if (timeToRead < 2000) {
-                timeToRead = 2000;
+            
+            if (!timeToRead) {
+                words = sentence.split(' ').length;
+                timeToRead = words * 300;
+                if (timeToRead < 2000) {
+                    timeToRead = 2000;
+                }
             }
             readDelay = timeToRead + _this.animDuration + _this.animDelay;
 
@@ -54,12 +66,16 @@ var Acon = {
         } else {
             el.classList.add('active');
         }
+    },
+    initCustomFunction: function (userDefinedFunction) {
+        var tmpFunc = new Function(userDefinedFunction);
+        tmpFunc();
     }
 }
 
 var test =  [
-    ['Hello', 'bottom', 3000],
-    ['Text from top','top'],
+    ['Hello', 'bottom', 100],
+    ['Text from top', 'top', 'console.log("test")'],
     'Default text',
     'Still a test',
     ['Text from left','left'],
